d172f48d5bd05ea7104dbef4fd8c07802c24987f
neerajdas7 <neerajdas@qburst.com>
2021-04-27 20:33:13 +0530

    Added firebase config files

---
+++ src/firebase/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import FIREBASE_CONFIG from '../config/firebaseConfig'
+
+
+export const firebaseConfig = firebase.initializeApp(FIREBASE_CONFIG)
+const baseDb = firebaseConfig.firestore();
+export const db = baseDb;
