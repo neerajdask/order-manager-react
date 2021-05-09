f44aa1af484b49b4eb19fcaabe4a9e23b4527a6a
neerajdas7 <neerajdas@qburst.com>
2021-05-09 12:35:51 +0530

    Test deployment FE

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://construyo-express-backend.herokuapp.com/";
 
 export const API = axios.create({
   baseURL: BASE_URL,
