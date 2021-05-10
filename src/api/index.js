b21be7ff684cd42f94166a1cc07fed9f03a7949f
neerajdas7 <neerajdas@qburst.com>
2021-05-10 20:45:26 +0530

    Prod push

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
-import axios from 'axios'
-export const BASE_URL = "http://localhost:5000";
-// export const BASE_URL = "https://construyo-express-backend.herokuapp.com/";
+import axios from 'axios';
+
+/**
+ * For development and debugging, set the baseURL to 'http://localhost:5000'
+ */
+
+export const BASE_URL = "https://construyo-express-backend.herokuapp.com/";
 
 export const API = axios.create({
   baseURL: BASE_URL,
